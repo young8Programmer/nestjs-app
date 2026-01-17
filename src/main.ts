f6114e38ab08cd7ba4334cbaa838981e8fd30b61
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:31:00 +0500

    chore: build konfiguratsiyasi sozlandi

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // database testlari qo'shildi
+// CORS xatosi tuzatildi
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
