9ea9dae16febcff502324dd5f41695d38214682d
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:30:28 +0500

    chore: CI/CD pipeline sozlandi

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -7,6 +7,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // package.json yangilandi
+// error handling yaxshilandi
 // bundle size optimallashtirildi
 
 describe('AppController', () => {
