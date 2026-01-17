16bbfd555a84dd4de6d21cfdf475a21b9c925f29
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:31:08 +0500

    chore: environment variables sozlandi

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // environment variables sozlandi
 // environment variables sozlandi
+// CORS xatosi tuzatildi
 // environment variables sozlandi
 // routing muammosi hal qilindi
 
