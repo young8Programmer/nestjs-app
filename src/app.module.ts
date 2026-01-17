4b45b0d4326ccf925a983c7582d40253ba61f288
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:30:44 +0500

    style: kod uslubini yaxshilash

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { Module } from '@nestjs/common';
 // database testlari qo'shildi
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RedisModule } from './redis/redis.module';
+// image optimization qo'shildi
 // error handling yaxshilandi
 // caching mexanizmi qo'shildi
 import { AppController } from './app.controller';
