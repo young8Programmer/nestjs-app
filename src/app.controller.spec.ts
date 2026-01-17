5938cc9d6335226eb94eb01a72b6982917bacd67
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:30:27 +0500

    test: unit testlar qo'shildi

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -5,6 +5,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // package.json yangilandi
+// bundle size optimallashtirildi
 
 describe('AppController', () => {
   let appController: AppController;
