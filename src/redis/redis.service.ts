033d35abb97e944496c766c7cbce5b24cdba9256
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:31:19 +0500

    perf: image optimization qo'shildi

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
 // ESLint qoidalariga moslashtirish
 
+// prettier formatlash
 @Injectable()
 // kod strukturasini yaxshilash
 export class RedisService {
