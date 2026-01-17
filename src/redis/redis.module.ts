5992ce8840993e689cea6e8210166aabcea9c686
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:31:10 +0500

    style: prettier formatlash

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 // code comments qo'shildi
 // installation qo'llanmasi yaratildi
+// caching mexanizmi qo'shildi
 // package.json yangilandi
 
 @Module({
