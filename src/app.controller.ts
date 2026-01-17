0fd7a9e03a398efcd6455b8e0c245425cea27fd7
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:30:33 +0500

    feat: database migrations yaratildi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+// componentlarni qayta tashkilash
   @Get()
   getHello(): string {
 // README faylini yangilash
